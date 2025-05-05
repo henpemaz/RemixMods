69ebee69d6f1769a09221573b7ffb8e04b555982
Kert <kertwaii@gmail.com>
2025-05-05 08:24:08 +0600

    Updated target game version

--- SplitScreenCoop/SplitScreenCoop.cs
+++ SplitScreenCoop/SplitScreenCoop.cs
@@ -19,7 +19,7 @@ using System.Runtime.CompilerServices;
 
 namespace SplitScreenCoop
 {
-    [BepInPlugin("com.henpemaz.splitscreencoop", "SplitScreen Co-op", "0.1.19")]
+    [BepInPlugin("com.henpemaz.splitscreencoop", "SplitScreen Co-op", "0.1.20")]
     public partial class SplitScreenCoop : BaseUnityPlugin
     {
         public static SplitScreenCoopOptions Options;
