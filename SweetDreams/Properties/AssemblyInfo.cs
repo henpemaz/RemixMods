ad4a7072fcea10f509896f9b0d9b0a8d1c6d64e9
glebi574 <glebi574@gmail.com>
2025-05-26 10:54:17 +0300

    Updated for 1.10
    * Rewrote the whole thing 🦊
    * Now Rain World's `JSON` is used instead of `Newtonsoft`
    * Removed `RestartGame` hook, as it didn't seem to do anything

--- SweetDreams/Properties/AssemblyInfo.cs
+++ SweetDreams/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -33,3 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
--- SweetDreams/SweetDreams.cs
+++ SweetDreams/SweetDreams.cs
@@ -1,322 +1,181 @@
-﻿using BepInEx;
+﻿// #define _DEBUG
+
+using BepInEx;
 using Menu;
-using Newtonsoft.Json;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Security;
-using System.Security.Permissions;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
-
-[assembly: AssemblyTrademark("Intikus, Tealppup & Henpemaz")]
-[module: UnverifiableCode]
-#pragma warning disable CS0618 // Type or member is obsolete
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+using static SweetDreams.Plugin;
+using Debug = UnityEngine.Debug;
 
 namespace SweetDreams
 {
-    [BepInPlugin("com.henpemaz.sweetdreams", "Sweet Dreams", "0.1.2")]
-    public class SweetDreams : BaseUnityPlugin
+  [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+    public const string PLUGIN_GUID = "henpemaz_sweetdreams";
+    public const string PLUGIN_NAME = "Sweet Dreams";
+    public const string PLUGIN_VERSION = "0.2.0";
+
+    public void OnEnable()
     {
-        public void OnEnable()
-        {
-            Logger.LogInfo("OnEnable");
-            On.RainWorld.OnModsInit += OnModsInit;
-            On.RainWorld.PostModsInit += RainWorld_PostModsInit;
-        }
-
-        private void RainWorld_PostModsInit(On.RainWorld.orig_PostModsInit orig, RainWorld self)
-        {
-            orig(self);
-            try
-            {
-                Logger.LogInfo("PostModsInit");
-                LoadSlugsAndDreams(self);
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-                throw;
-            }
-        }
-
-        //void Update() // debug thinghies
-        //{
-        //    if (Input.GetKeyDown("1"))
-        //    {
-        //        if (GameObject.FindObjectOfType<RainWorld>().processManager.currentMainLoop is RainWorldGame game)
-        //            game.Win(false);
-        //    }
-        //}
-
-        public bool init;
-        public void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-        {
-            try
-            {
-                if (init) return;
-                init = true;
-
-                Logger.LogInfo("OnModsInit");
-                On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
-                On.RainWorldGame.RestartGame += RainWorldGame_RestartGame;
-                Logger.LogInfo("OnModsInit done");
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-                throw;
-            }
-            finally
-            {
-                orig(self);
-            }
-        }
-
-        private void RainWorldGame_RestartGame(On.RainWorldGame.orig_RestartGame orig, RainWorldGame self)
-        {
-            LoadSlugsAndDreams(self.rainWorld);
-            orig(self);
-        }
-
-        public class Vector2Converter : JsonConverter
-        {
-            public override bool CanConvert(Type objectType)
-            {
-                return objectType == typeof(Vector2?);
-            }
-
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-            {
-                var t = serializer.Deserialize<double[]>(reader);
-                if (t == null) return new Vector2?();
-                return new Vector2?(new Vector2((float)t[0], (float)t[1]));
-            }
-
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-                Vector2? v = (Vector2?)value;
-                if (!v.HasValue) return;
-                writer.WriteStartArray();
-                writer.WriteValue(v.Value.x);
-                writer.WriteValue(v.Value.y);
-                writer.WriteEndArray();
-            }
-        }
-
-        public class ExtEnumConverter : JsonConverter
-        {
-            public override bool CanConvert(Type objectType)
-            {
-                return typeof(ExtEnumBase).IsAssignableFrom(objectType);
-            }
-
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-            {
-                var value = serializer.Deserialize<string>(reader);
-                return value != null && ExtEnumBase.TryParse(objectType, value, true, out var val) ? val : null;
-            }
-
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-                if (value == null) writer.WriteNull();
-                else writer.WriteValue(value.ToString());
-            }
-        }
-
-        public static Dictionary<string, SlugDreams> dreamtionary;
-        public void LoadSlugsAndDreams(RainWorld rw)
-        {
-            dreamtionary = new();
-            var jsonSerializer = new JsonSerializer();
-            jsonSerializer.Converters.Add(new Vector2Converter());
-            jsonSerializer.Converters.Add(new ExtEnumConverter());
-            foreach (var s in AssetManager.ListDirectory("sweetdreams"))
-            {
-                Logger.LogInfo("reading file " + s);
-                try
-                {
-                    var read = (Dictionary<string, SlugDreams>)jsonSerializer.Deserialize(File.OpenText(AssetManager.ResolveFilePath(s)), typeof(Dictionary<string, SlugDreams>));
-                    UpdateFromOther(dreamtionary, read);
-                }
-                catch (Exception e)
-                {
-                    Logger.LogError("Error reading file " + s);
-                    Logger.LogError(e);
-                    //throw;
-                }
-            }
-
-            try
-            {
-                Logger.LogInfo(JsonConvert.SerializeObject(dreamtionary, Formatting.Indented, new Vector2Converter(), new ExtEnumConverter()));
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-                throw;
-            }
-        }
-
-        public interface IUpdatebleFromOther
-        {
-            void UpdateFromOther(IUpdatebleFromOther other);
-        }
-        public static void UpdateFromOther(IDictionary self, IDictionary other)
-        {
-            foreach (var key in other.Keys)
-            {
-                if (self.Contains(key))
-                {
-                    var obj = self[key];
-                    if (obj is IUpdatebleFromOther iufo) iufo.UpdateFromOther((IUpdatebleFromOther)other[key]);
-                    else if (obj is IDictionary idi) UpdateFromOther(idi, (IDictionary)other[key]);
-                    else self[key] = other[key];
-                }
-                else
-                {
-                    self[key] = other[key];
-                }
-            }
-        }
-
-        public class SlugDreams : IUpdatebleFromOther
-        {
-            public Dictionary<string, Dream> dreams = new();
-            public SlugcatStats.Name fallback;
-            public string sleepingSprite;
-
-            internal bool TryGetValue(CreatureTemplate.Type type, out Dream dream)
-            {
-                return this.dreams.TryGetValue(type.value, out dream) || (fallback != null && dreamtionary.TryGetValue(fallback.value, out var other) && other.TryGetValue(type, out dream));
-            }
-
-            public void UpdateFromOther(IUpdatebleFromOther other)
-            {
-                if (other is SlugDreams sd)
-                {
-                    SweetDreams.UpdateFromOther(dreams, sd.dreams);
-                    fallback = sd.fallback ?? fallback;
-                    sleepingSprite = sd.sleepingSprite ?? sd.sleepingSprite;
-                }
-            }
-        }
-
-        public class Dream : IUpdatebleFromOther
-        {
-            public string song;
-            public Dictionary<string, DreamLayer> layers = new();
-
-            public void UpdateFromOther(IUpdatebleFromOther other)
-            {
-                if (other is Dream sd)
-                {
-                    song = sd.song ?? song;
-                    SweetDreams.UpdateFromOther(layers, sd.layers);
-                }
-            }
-        }
-
-        public class DreamLayer : IUpdatebleFromOther
-        {
-            public string sprite;
-            public bool? onTop;
-            public Vector2? pos;
-            public float? slugDepthOffset;
-            public MenuDepthIllustration.MenuShader shader;
-
-            public void UpdateFromOther(IUpdatebleFromOther other)
-            {
-                if (other is DreamLayer sd)
-                {
-                    sprite = sd.sprite ?? sprite;
-                    onTop = sd.onTop ?? onTop;
-                    pos = sd.pos ?? pos;
-                    slugDepthOffset = sd.slugDepthOffset ?? slugDepthOffset;
-                    shader = sd.shader ?? shader;
-                }
-            }
-        }
-
-        private void SleepAndDeathScreen_GetDataFromGame(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
-        {
-            orig(self, package);
-            // sleep and death, but we only care about sleep
-            if (!self.IsSleepScreen)
-            {
-                return;
-            }
-            var slugcat = GetSlugInDen(self, package);
-            if (slugcat == null)
-            {
-                Logger.LogError("couldn't find slugcat!");
-                return;
-            }
-            var slugstration = GetIllustrationOfSlug(self, slugcat);
-            if (slugstration == null)
-            {
-                Logger.LogError("couldn't find slugcat's illustration in the scene!");
-                return;
-            }
-            var friendInDen = GetFriendInDen(self, package);
-            if (friendInDen == null)
-            {
-                Logger.LogInfo("couldn't find friend in den!");
-                return;
-            }
-            var dream = GetDreamOfFriend(package, slugcat, friendInDen);
-            if (dream == null)
-            {
-                Logger.LogInfo($"no dream for {slugcat} and {friendInDen}");
-                return;
-            }
-
-            if (!string.IsNullOrEmpty(dream.song) && self.manager.musicPlayer != null)
-            {
-                self.manager.musicPlayer.MenuRequestsSong(dream.song, 0.5f, 10f);
-            }
-
-            foreach (var layer in dream.layers.Values)
-            {
-                var stration = new MenuDepthIllustration(self, self.scene, "illustrations" + Path.DirectorySeparatorChar + "sweetdreams", layer.sprite, layer.pos ?? Vector2.zero, slugstration.depth + layer.slugDepthOffset ?? 0, layer.shader ?? MenuDepthIllustration.MenuShader.Basic);
-                self.scene.AddIllustration(stration);
-                if (layer.onTop ?? true) stration.sprite.MoveInFrontOfOtherNode(slugstration.sprite);
-                else stration.sprite.MoveBehindOtherNode(slugstration.sprite);
-            }
-        }
-
-        public SlugcatStats.Name GetSlugInDen(SleepAndDeathScreen sads, KarmaLadderScreen.SleepDeathScreenDataPackage package)
-        {
-            return package.characterStats.name;
-        }
-
-        public AbstractCreature GetFriendInDen(SleepAndDeathScreen sads, KarmaLadderScreen.SleepDeathScreenDataPackage package)
-        {
-            return package.sessionRecord.friendInDen;
-        }
-
-        public Dream GetDreamOfFriend(KarmaLadderScreen.SleepDeathScreenDataPackage package, SlugcatStats.Name slugcat, AbstractCreature friendInDen)
-        {
-            if (dreamtionary.TryGetValue(slugcat.value, out var listOfDreams))
-            {
-                if (listOfDreams.TryGetValue(friendInDen.creatureTemplate.type, out Dream dream))
-                {
-                    return dream;
-                }
-            }
-            return null;
-        }
-
-        public MenuDepthIllustration GetIllustrationOfSlug(SleepAndDeathScreen sads, SlugcatStats.Name slug)
-        {
-            if (dreamtionary.TryGetValue(slug.value, out var slugDreams) && !string.IsNullOrEmpty(slugDreams.sleepingSprite))
-            {
-                return sads.scene.depthIllustrations.FirstOrDefault(v => v.fileName.ToLowerInvariant() == slugDreams.sleepingSprite.ToLowerInvariant());
-            }
-            return null;
-        }
+      On.RainWorld.OnModsInit += RainWorld_OnModsInit;
     }
+
+    public void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+      orig(self);
+
+      On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
+
+      LoadData(self);
+
+#if _DEBUG
+      On.RainWorldGame.Update += RainWorldGame_Update;
+#endif
+    }
+
+#if _DEBUG
+    public void RainWorldGame_Update(On.RainWorldGame.orig_Update orig, RainWorldGame self)
+    {
+      orig(self);
+      if (Input.GetKey("b"))
+        self.Win(false, false);
+    }
+#endif
+
+    public void SleepAndDeathScreen_GetDataFromGame(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
+    {
+      orig(self, package);
+
+      if (!self.IsSleepScreen)
+        return;
+
+      string slugcatName = package.characterStats.name.value;
+      if (slugcatName == null)
+      {
+        Logger.LogError("Failed to find slugcat in the shelter");
+        return;
+      }
+
+      if (!dreams.TryGetValue(slugcatName, out SlugcatDreams slugcatDreams))
+      {
+        Logger.LogError("Failed to find slugcat's illustration in loaded scenes");
+        return;
+      }
+
+      MenuDepthIllustration menuDepthIllustration = self.scene.depthIllustrations
+        .FirstOrDefault(v => v.fileName.ToLowerInvariant() == slugcatDreams.sleepingSprite.ToLowerInvariant());
+      if (menuDepthIllustration == null)
+      {
+        Logger.LogError("Failed to find slugcat's illustration in current scene");
+        return;
+      }
+
+      AbstractCreature friend = package.sessionRecord.friendInDen;
+      if (friend == null)
+      {
+        Logger.LogWarning("Failed to find friend in the den");
+        return;
+      }
+
+      if (!dreams[slugcatName].dreams.TryGetValue(friend.creatureTemplate.type.value, out Dream dream))
+      {
+        Logger.LogError($"No dream was found for {slugcatName} and {friend.creatureTemplate.type.value}");
+        return;
+      }
+
+      if (!dream.song.IsNullOrWhiteSpace())
+      {
+        self.manager.musicPlayer?.MenuRequestsSong(dream.song, 0.5f, 10f);
+      }
+
+      foreach (DreamLayer layer in dream.layers)
+      {
+        MenuDepthIllustration customIllustration =
+          new(self, self.scene, "illustrations\\sweetdreams", layer.sprite, layer.pos,
+          menuDepthIllustration.depth + layer.slugDepthOffset, layer.shader);
+        self.scene.AddIllustration(customIllustration);
+        if (layer.onTop)
+          customIllustration.sprite.MoveInFrontOfOtherNode(menuDepthIllustration.sprite);
+        else
+          customIllustration.sprite.MoveBehindOtherNode(menuDepthIllustration.sprite);
+      }
+    }
+
+    public class DreamLayer
+    {
+      public string sprite;
+      public float slugDepthOffset;
+      public bool onTop = false;
+      public Vector2 pos = Vector2.zero;
+      public MenuDepthIllustration.MenuShader shader = MenuDepthIllustration.MenuShader.Basic;
+
+      public DreamLayer(Dictionary<string, object> layer)
+      {
+        sprite = (string)layer["sprite"];
+        shader ??= typeof(MenuDepthIllustration.MenuShader)
+          .GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
+          .FirstOrDefault(v => v.Name.ToLowerInvariant() == (layer["shader"] as string).ToLowerInvariant())
+          .GetValue(null) as MenuDepthIllustration.MenuShader;
+        slugDepthOffset = (float)(double)layer["slugDepthOffset"];
+        onTop = (bool)layer["onTop"];
+        if (layer.TryGetValue("pos", out object obj) && obj is List<object> coordinates)
+          pos = new(Convert.ToInt32(coordinates[0]), Convert.ToInt32(coordinates[1]));
+      }
+    }
+
+    public class Dream
+    {
+      public string song;
+      public List<DreamLayer> layers = new();
+
+      public Dream(KeyValuePair<string, object> creatureDream)
+      {
+        song = (string)(creatureDream.Value as Dictionary<string, object>)["song"];
+        Dictionary<string, object> parsedLayers = (creatureDream.Value as Dictionary<string, object>)["layers"] as Dictionary<string, object>;
+        layers.Add(new DreamLayer(parsedLayers["bottom"] as Dictionary<string, object>));
+        if (parsedLayers.TryGetValue("top", out object layer))
+          layers.Add(new DreamLayer(layer as Dictionary<string, object>));
+      }
+    }
+
+    public class SlugcatDreams
+    {
+      public string sleepingSprite;
+      public Dictionary<string, Dream> dreams = new();
+
+      public SlugcatDreams(KeyValuePair<string, object> dream)
+      {
+        sleepingSprite = (string)(dream.Value as Dictionary<string, object>)["sleepingSprite"];
+        foreach (KeyValuePair<string, object> creatureDream in (dream.Value as Dictionary<string, object>)["dreams"] as Dictionary<string, object>)
+          dreams[creatureDream.Key] = new(creatureDream);
+      }
+    }
+
+    public static Dictionary<string, SlugcatDreams> dreams = new();
+
+    public void LoadData(RainWorld self)
+    {
+      foreach (string path in AssetManager.ListDirectory("sweetdreams"))
+      {
+        try
+        {
+          Logger.LogInfo($"Reading at: {path}");
+          foreach (KeyValuePair<string, object> slugcatDream in Json.Parser.Parse(File.ReadAllText(path)) as Dictionary<string, object>)
+            dreams[slugcatDream.Key] = new(slugcatDream);
+        }
+        catch (Exception e)
+        {
+          Logger.LogError(e);
+        }
+      }
+      Logger.LogInfo($"Finished reading");
+    }
+  }
 }
