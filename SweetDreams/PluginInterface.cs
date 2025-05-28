dd40a168dbd6d07d2e166b922e2933c9b144f658
glebi574 <glebi574@gmail.com>
2025-05-28 20:47:34 +0300

    Added support for all creatures
    * Now any creature can be used in the dream
    * Now dream can show multiple creatures of the same type(for each creature of that type, present in the den)
    * Added remix menu, which allows to control new system for multiple creatures. For now allows to select, which creature to search for in dream - pups, lizards or any supported creature.
    * Added dreams for pups(up to 3 are shown via new system; no songs)
    * Removed `RestartGame` hook - now, while debugging dreams data is automatically reloaded, which simplifies testing
    * Fixed dream files from other mods sometimes loading first
    * Improved compatibility with mods, adding their own shaders

---
+++ SweetDreams/PluginInterface.cs
@@ -0,0 +1,43 @@
+﻿using Menu.Remix.MixedUI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static SweetDreams.PluginInterface;
+
+namespace SweetDreams
+{
+  public class PluginInterface : OptionInterface
+  {
+    // To specify preference of creature in dream selection
+    public enum PreferenceMode
+    {
+      Any,
+      Pups,
+      Lizards,
+    }
+
+    public readonly Configurable<PreferenceMode> preferenceMode;
+
+    private UIelement[] options;
+
+    public PluginInterface()
+    {
+      preferenceMode = config.Bind("preferenceMode", PreferenceMode.Any);
+    }
+
+    public override void Initialize()
+    {
+      OpTab optionsTab = new OpTab(this, "Options");
+      Tabs = new OpTab[] { optionsTab };
+
+      options = new UIelement[] {
+        new OpLabel(10f, 550f, "Options", bigText: true),
+
+        new OpLabel(10f, 510f, "Creature preference in dreams", bigText: false),
+        new OpComboBox(preferenceMode, new Vector2(10f, 480f), 130f, OpResourceSelector.GetEnumNames(null, typeof(PreferenceMode)).ToList()),
+      };
+
+      optionsTab.AddItems(options);
+    }
+  }
+}
--- SweetDreams/SweetDreams.cs
+++ SweetDreams/SweetDreams.cs
@@ -2,6 +2,7 @@
 
 using BepInEx;
 using Menu;
+using MoreSlugcats;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using PreferenceMode = SweetDreams.PluginInterface.PreferenceMode;
 
 namespace SweetDreams
 {
@@ -38,7 +40,7 @@ namespace SweetDreams
     }
 
     // TryGetValueWithType, where retrieved element is searched in specified class' static fields
-    public static bool TryGetStatic<T>(this Dictionary<string, object> self, string name, out T output)
+    public static bool TryGetStatic<T>(this Dictionary<string, object> self, string name, out T output) where T : ExtEnum<T>
     {
       if (self.TryGetValueWithType(name, out string fieldName) && Plugin.GetStaticElement<T>(fieldName) is T value)
       {
@@ -49,7 +51,7 @@ namespace SweetDreams
       return false;
     }
 
-    public static void TrySetStatic<T>(this Dictionary<string, object> self, string name, ref T output)
+    public static void TrySetStatic<T>(this Dictionary<string, object> self, string name, ref T output) where T : ExtEnum<T>
     {
       if (self.TryGetValueWithType(name, out string fieldName) && Plugin.GetStaticElement<T>(fieldName) is T value)
         output = value;
@@ -76,12 +78,28 @@ namespace SweetDreams
     }
   }
 
+  public static class ExtList
+  {
+    public static List<AbstractCreature> GetCreaturesWithType(this List<AbstractCreature> creatures, CreatureTemplate.Type type)
+    {
+      List<AbstractCreature> result = new();
+      foreach (AbstractCreature creature in creatures)
+        if (creature.creatureTemplate.type == type)
+          result.Add(creature);
+      return result;
+    }
+  }
+
   [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
   public class Plugin : BaseUnityPlugin
   {
     public const string PLUGIN_GUID = "henpemaz_sweetdreams";
     public const string PLUGIN_NAME = "Sweet Dreams";
     public const string PLUGIN_VERSION = "0.2.0";
+
+    public PluginInterface pluginInterface;
+
+    public PreferenceMode preferenceMode => pluginInterface.preferenceMode.Value;
 
     public void OnEnable()
     {
@@ -93,13 +111,15 @@ namespace SweetDreams
       try
       {
         On.Menu.SleepAndDeathScreen.GetDataFromGame += SleepAndDeathScreen_GetDataFromGame;
-        On.RainWorldGame.RestartGame += RainWorldGame_RestartGame;
 
 #if _DEBUG
         On.RainWorldGame.Update += RainWorldGame_Update;
 #endif
 
         LoadData();
+
+        pluginInterface = new PluginInterface();
+        MachineConnector.SetRegisteredOI(PLUGIN_GUID, pluginInterface);
       }
       catch (Exception e)
       {
@@ -113,16 +133,16 @@ namespace SweetDreams
     public void RainWorldGame_Update(On.RainWorldGame.orig_Update orig, RainWorldGame self)
     {
       orig(self);
-      if (Input.GetKey("b"))
+      if (Input.GetKey("n"))
+      {
+        foreach (AbstractRoom room in self.world.abstractRooms)
+          foreach (AbstractCreature creature in room.creatures)
+            if (creature.realizedCreature is Player player)
+              player.AddFood(16);
         self.Win(false, false);
+      }
     }
 #endif
-
-    public void RainWorldGame_RestartGame(On.RainWorldGame.orig_RestartGame orig, RainWorldGame self)
-    {
-      LoadData();
-      orig(self);
-    }
 
     public void SleepAndDeathScreen_GetDataFromGame(On.Menu.SleepAndDeathScreen.orig_GetDataFromGame orig, SleepAndDeathScreen self, KarmaLadderScreen.SleepDeathScreenDataPackage package)
     {
@@ -130,6 +150,10 @@ namespace SweetDreams
 
       if (!self.IsSleepScreen)
         return;
+
+#if _DEBUG
+      LoadData();
+#endif
 
       string slugcatName = package.characterStats.name.value;
       if (slugcatName == null)
@@ -152,53 +176,101 @@ namespace SweetDreams
         return;
       }
 
-      AbstractCreature friend = package.sessionRecord.friendInDen;
-      if (friend == null)
+      List<AbstractCreature> denCreatures = package.mapData.world.abstractRooms
+        .FirstOrDefault(v => v.name == package.saveState.denPosition).creatures;
+      if (denCreatures.Count < 2)
       {
-        Logger.LogWarning("Failed to find friend in the den");
+        Logger.LogWarning("Failed to find friend in room - too little creatures");
         return;
+      }
+      bool foundFriends = false;
+
+      Dream dream = null;
+      switch (preferenceMode)
+      {
+        case PreferenceMode.Any:
+          foreach (AbstractCreature creature in denCreatures)
+          {
+            if (creature.creatureTemplate.type == CreatureTemplate.Type.Slugcat && creature.abstractAI == null
+              || !slugcatDreams.dreams.TryGetValue(creature.creatureTemplate.type.value, out dream))
+              continue;
+            denCreatures = denCreatures.GetCreaturesWithType(creature.creatureTemplate.type);
+            foundFriends = true;
+            break;
+          }
+          break;
+        case PreferenceMode.Pups:
+          foreach (AbstractCreature creature in denCreatures)
+          {
+            if (creature.abstractAI.RealAI is not SlugNPCAI
+              || !slugcatDreams.dreams.TryGetValue(creature.creatureTemplate.type.value, out dream))
+              continue;
+            denCreatures = denCreatures.GetCreaturesWithType(MoreSlugcatsEnums.CreatureTemplateType.SlugNPC);
+            denCreatures.RemoveAll(v => v.abstractAI == null);
+            foundFriends = true;
+            break;
+          }
+          break;
+        case PreferenceMode.Lizards:
+          foreach (AbstractCreature creature in denCreatures)
+          {
+            if (creature.abstractAI?.RealAI is not LizardAI
+              || !slugcatDreams.dreams.TryGetValue(creature.creatureTemplate.type.value, out dream))
+              continue;
+            denCreatures = denCreatures.GetCreaturesWithType(creature.creatureTemplate.type);
+            foundFriends = true;
+          }
+          break;
       }
 
-      if (!slugcatDreams.dreams.TryGetValue(friend.creatureTemplate.type.value, out Dream dream)
-        && !slugcatDreams.dreams.TryGetValue(slugcatDreams.fallback, out dream))
+      Dream fallbackDream = null;
+      if (!foundFriends || slugcatDreams.fallback != null && !slugcatDreams.dreams.TryGetValue(slugcatDreams.fallback, out fallbackDream))
       {
-        Logger.LogError($"No dream was found for {slugcatName} and {friend.creatureTemplate.type.value}");
+        Logger.LogWarning("Failed to find friend in room - no friends were found");
         return;
       }
+      dream ??= fallbackDream;
 
       if (!dream.song.IsNullOrWhiteSpace())
       {
         self.manager.musicPlayer?.MenuRequestsSong(dream.song, 0.5f, 10f);
       }
 
-      foreach (KeyValuePair<string, DreamLayer> layerData in dream.layers)
-      {
-        DreamLayer layer = layerData.Value;
-        MenuDepthIllustration customIllustration =
-          new(self, self.scene, "illustrations\\sweetdreams", layer.sprite, layer.pos ?? Vector2.zero,
-          menuDepthIllustration.depth + (layer.slugDepthOffset ?? 0f), layer.shader);
-        self.scene.AddIllustration(customIllustration);
-        if (layer.onTop ?? true)
-          customIllustration.sprite.MoveInFrontOfOtherNode(menuDepthIllustration.sprite);
-        else
-          customIllustration.sprite.MoveBehindOtherNode(menuDepthIllustration.sprite);
-      }
+      self.scene.depthIllustrations[self.scene.depthIllustrations.Count - 1].pos.y -= 80f;
+      List<KeyValuePair<DreamLayer, FSprite>> customSprites = new();
+      for (int i = 1; i <= Math.Min(denCreatures.Count, dream.layers.Count); ++i)
+        foreach (KeyValuePair<string, DreamLayer> layerData in dream.layers[i.ToString()])
+        {
+          DreamLayer layer = layerData.Value;
+          MenuDepthIllustration customIllustration =
+            new(self, self.scene, "illustrations\\sweetdreams", layer.sprite, layer.pos ?? Vector2.zero,
+            menuDepthIllustration.depth + (layer.slugDepthOffset ?? 0f), layer.shader);
+          self.scene.AddIllustration(customIllustration);
+          if (layer.onTop ?? true)
+            customIllustration.sprite.MoveInFrontOfOtherNode(menuDepthIllustration.sprite);
+          else
+            customIllustration.sprite.MoveBehindOtherNode(menuDepthIllustration.sprite);
+          if (layer.order != null)
+            customSprites.Add(new(layer, customIllustration.sprite));
+        }
+      customSprites.Sort((a, b) => (a.Key.order ?? 1).CompareTo(b.Key.order));
+      for (int i = 1; i < customSprites.Count; ++i)
+        if (customSprites[i - 1].Key.onTop == customSprites[i].Key.onTop)
+          customSprites[i].Value.MoveInFrontOfOtherNode(customSprites[i - 1].Value);
     }
 
     // Returns static field with given name from specified class
-    public static T GetStaticElement<T>(string name)
+    public static T GetStaticElement<T>(string name) where T : ExtEnum<T>
     {
-      object value = typeof(T)
-        .GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
-        .FirstOrDefault(v => v.Name.ToLowerInvariant() == name.ToLowerInvariant())
-        ?.GetValue(null);
-      return value == null ? default : (T)value;
+      ExtEnumBase.TryParse(typeof(T), name, true, out ExtEnumBase result);
+      return result == null ? default : (T)result;
     }
 
     public class DreamLayer : IUpdatableFromOther
     {
       public string sprite;
       public MenuDepthIllustration.MenuShader shader;
+      public int? order;
       public float? slugDepthOffset;
       public bool? onTop;
       public Vector2? pos;
@@ -206,8 +278,10 @@ namespace SweetDreams
       public DreamLayer(Dictionary<string, object> layer)
       {
         layer.TrySetValueWithType("sprite", ref sprite);
-        layer.TrySetStatic("shader", ref shader);
+        layer.TryGetValueWithType("order", out Int64 orderValue);
+          order = (int)orderValue;
         layer.TrySetValueWithType("onTop", ref onTop);
+        layer.TrySetStatic("shader", ref shader);
         if (layer.TryGetValueWithType("slugDepthOffset", out double depth))
           slugDepthOffset = (float)depth;
         if (layer.TryGetValueWithType("pos", out List<object> coordinates))
@@ -219,8 +293,9 @@ namespace SweetDreams
         if (other is not DreamLayer layer)
           return;
         sprite = layer.sprite ?? sprite;
-        shader = layer.shader ?? shader;
+        order = layer.order ?? order;
         onTop = layer.onTop ?? onTop;
+        shader = layer.shader ?? shader;
         slugDepthOffset = layer.slugDepthOffset ?? slugDepthOffset;
         pos = layer.pos ?? pos;
       }
@@ -230,17 +305,24 @@ namespace SweetDreams
     public class Dream : IUpdatableFromOther
     {
       public string song;
-      public Dictionary<string, DreamLayer> layers = new();
+      public Dictionary<string, Dictionary<string, DreamLayer>> layers = new();
 
       public Dream(Dictionary<string, object> creatureDream)
       {
         creatureDream.TrySetValueWithType("song", ref song);
-        if (!creatureDream.TryGetValueWithType("layers", out Dictionary<string, object> parsedLayers))
+        if (!creatureDream.TryGetValueWithType("layers", out Dictionary<string, object> indexedLayers))
           return;
-        if (parsedLayers.TryGetValueWithType("bottom", out Dictionary<string, object> bottom))
-          layers["bottom"] = new(bottom);
-        if (parsedLayers.TryGetValueWithType("top", out Dictionary<string, object> top))
-          layers["top"] = new(top);
+        foreach (KeyValuePair<string, object> indexedLayer in indexedLayers)
+        {
+          if (indexedLayer.Value is not Dictionary<string, object> parsedLayers)
+            continue;
+          Dictionary<string, DreamLayer> actualLayers = new();
+          if (parsedLayers.TryGetValueWithType("bottom", out Dictionary<string, object> bottom))
+            actualLayers["bottom"] = new(bottom);
+          if (parsedLayers.TryGetValueWithType("top", out Dictionary<string, object> top))
+            actualLayers["top"] = new(top);
+          layers[indexedLayer.Key] = actualLayers;
+        }
       }
 
       public void Update(IUpdatableFromOther other)
@@ -283,7 +365,8 @@ namespace SweetDreams
       allDreams = new();
       try
       {
-        foreach (string path in AssetManager.ListDirectory("sweetdreams"))
+        foreach (string path in AssetManager.ListDirectory("sweetdreams")
+          .OrderBy(v => !Path.GetFileName(v).Equals("main.json")))
         {
           Logger.LogInfo($"Reading at: {path}");
           foreach (KeyValuePair<string, object> slugcatDreamData in Json.Parser.Parse(File.ReadAllText(path)) as Dictionary<string, object>)
